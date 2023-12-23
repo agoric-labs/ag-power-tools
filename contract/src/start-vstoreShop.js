d28c84e908335e1152fa76c1659be9ba438a4cfa
Dan Connolly <dckc@madmode.com>
2023-12-23 00:13:09 -0600

    chore: move startVstoreShop from test/ to src/

---
+++ contract/src/start-vstoreShop.js
@@ -0,0 +1,77 @@
+// @ts-check
+
+import { E } from '@endo/far';
+
+const { entries, fromEntries } = Object;
+
+const { Fail } = assert;
+
+export const contractName = 'vstoreShop';
+
+/** @type { <T extends Record<string, ERef<any>>>(obj: T) => Promise<{ [K in keyof T]: Awaited<T[K]>}> } */
+const allValues = async obj => {
+  const es = await Promise.all(
+    entries(obj).map(async ([k, v]) => [k, await v]),
+  );
+  return fromEntries(es);
+};
+
+// XXX avoid linking
+const AmountMath = { make: (brand, value) => harden({ brand, value }) };
+
+/**
+ * @param {Brand} brand
+ * @param {number} n
+ * @param {number} decimalPlaces
+ */
+const units = (brand, n, decimalPlaces) =>
+  AmountMath.make(brand, BigInt(Math.round(n * 10 ** decimalPlaces)));
+
+/**
+ * @param {import('@agoric/vats').BootstrapPowers} powers
+ * @param {{ options?: { vstoreShop?: { bundleID?: string, priceUnits }}}} [config]
+ */
+export const startVstoreShop = async (powers, config = {}) => {
+  const { bundleID = Fail`missing bundleID`, priceUnits = 50 } =
+    config.options?.vstoreShop || {};
+  const {
+    consume: { zoe, chainStorage },
+    issuer: {
+      consume: { IST: istIssuerP },
+    },
+    brand: {
+      consume: { IST: istBrandP },
+    },
+    installation: {
+      produce: { [contractName]: produceInstallation },
+    },
+    instance: {
+      produce: { [contractName]: produceInstance },
+    },
+  } = powers;
+
+  /** @type {Installation<import('../src/vstoreShop.js').start>} */
+  const installation = await E(zoe).installBundleID(bundleID);
+
+  const { storageNode, istIssuer } = await allValues({
+    storageNode: E(chainStorage).makeChildNode(contractName),
+    istIssuer: istIssuerP,
+  });
+  const {
+    istBrand,
+    displayInfo: { decimalPlaces },
+  } = await allValues({
+    istBrand: istBrandP,
+    displayInfo: E(istBrandP).getDisplayInfo(),
+  });
+
+  produceInstallation.resolve(installation);
+
+  const started = await E(zoe).startInstance(
+    installation,
+    { Payment: istIssuer },
+    { basePrice: units(istBrand, priceUnits, decimalPlaces) },
+    { storageNode },
+  );
+  produceInstance.resolve(started.instance);
+};
--- contract/test/test-vstoreShop.js
+++ contract/test/test-vstoreShop.js
@@ -3,7 +3,7 @@ import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 import { createRequire } from 'node:module';
 import { E } from '@endo/far';
 import { makeNodeBundleCache } from '@endo/bundle-source/cache.js';
-import { makeIssuerKit, AmountMath } from '@agoric/ertp';
+import { makeIssuerKit } from '@agoric/ertp';
 import { makeFakeStorageKit } from '@agoric/internal/src/storage-test-utils.js';
 import { makePromiseSpace, makeNameHubKit } from '@agoric/vats';
 import { makeWellKnownSpaces } from '@agoric/vats/src/core/utils.js';
@@ -11,9 +11,7 @@ import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
 import { makeZoeForTest } from '@agoric/zoe/tools/setup-zoe.js';
 import { withAmountUtils } from './ertp-aux.js';
 
-const { Fail } = assert;
-
-const { fromEntries, entries, keys } = Object;
+import { contractName, startVstoreShop } from '../src/start-vstoreShop.js';
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<makeTestContext>>>} */
 const test = anyTest;
@@ -23,7 +21,6 @@ const myRequire = createRequire(import.meta.url);
 const assets = {
   vstoreShop: myRequire.resolve('../src/vstoreShop.js'),
 };
-const [contractName] = keys(assets);
 
 const makeTestContext = async () => {
   const bundleCache = await makeNodeBundleCache('bundles/', {}, s => import(s));
@@ -88,21 +85,18 @@ test('buy and write to storage', async t => {
 });
 
 /**
- * @param {Brand} brand
- * @param {number} n
- * @param {number} decimalPlaces
+ * Mock enough powers for startVstoreShop permit.
+ *
+ * @param {(...args: unknown[]) => void} log
  */
-const units = (brand, n, decimalPlaces) =>
-  AmountMath.make(brand, BigInt(Math.round(n * 10 ** decimalPlaces)));
-
 const mockBootstrap = async log => {
   const { produce, consume } = makePromiseSpace();
   const { admin, vatAdminState } = makeFakeVatAdmin();
   const zoe = makeZoeForTest(admin);
-  produce.zoe.resolve(zoe);
+  const feeIssuer = await E(zoe).getFeeIssuer();
+  const feeBrand = await E(feeIssuer).getBrand();
 
   const { rootNode: chainStorage, data } = makeFakeStorageKit('X');
-  produce.chainStorage.resolve(chainStorage);
 
   const { nameHub: agoricNames, nameAdmin: agoricNamesAdmin } =
     makeNameHubKit();
@@ -112,94 +106,33 @@ const mockBootstrap = async log => {
     'issuer',
     'brand',
   ]);
-  const feeIssuer = await E(zoe).getFeeIssuer();
-  const feeBrand = await E(feeIssuer).getBrand();
+
+  produce.zoe.resolve(zoe);
+  produce.chainStorage.resolve(chainStorage);
   spaces.issuer.produce.IST.resolve(feeIssuer);
   spaces.brand.produce.IST.resolve(feeBrand);
   const powers = { produce, consume, ...spaces };
+
   return { powers, vatAdminState, vstorageData: data };
-};
-
-/** @type { <T extends Record<string, ERef<any>>>(obj: T) => Promise<{ [K in keyof T]: Awaited<T[K]>}> } */
-const allValues = async obj => {
-  const es = await Promise.all(
-    entries(obj).map(async ([k, v]) => [k, await v]),
-  );
-  return fromEntries(es);
-};
-
-/**
- * @param {import('@agoric/vats').BootstrapPowers} powers
- * @param {{ options?: { vstoreShop?: { bundleID?: string, priceUnits }}}} [config]
- */
-const startVstoreShop = async (powers, config = {}) => {
-  const { bundleID = Fail`missing bundleID`, priceUnits = 50 } =
-    config.options?.vstoreShop || {};
-  const {
-    consume: { zoe, chainStorage },
-    issuer: {
-      consume: { IST: istIssuerP },
-    },
-    brand: {
-      consume: { IST: istBrandP },
-    },
-    installation: {
-      produce: { [contractName]: produceInstallation },
-    },
-    instance: {
-      produce: { [contractName]: produceInstance },
-    },
-  } = powers;
-
-  /** @type {Installation<import('../src/vstoreShop.js').start>} */
-  const installation = await E(zoe).installBundleID(bundleID);
-
-  const { storageNode, istIssuer } = await allValues({
-    storageNode: E(chainStorage).makeChildNode(contractName),
-    istIssuer: istIssuerP,
-  });
-  const {
-    istBrand,
-    displayInfo: { decimalPlaces },
-  } = await allValues({
-    istBrand: istBrandP,
-    displayInfo: E(istBrandP).getDisplayInfo(),
-  });
-
-  produceInstallation.resolve(installation);
-
-  const started = await E(zoe).startInstance(
-    installation,
-    { Payment: istIssuer },
-    { basePrice: units(istBrand, priceUnits, decimalPlaces) },
-    { storageNode },
-  );
-  produceInstance.resolve(started.instance);
 };
 
 test('start contract from bootstrap', async t => {
   t.log('bootstrap');
   const { powers, vatAdminState, vstorageData } = await mockBootstrap(t.log);
 
-  t.log('publish bundle');
   const { bundleCache } = t.context;
   const bundle = await bundleCache.load(assets.vstoreShop, contractName);
+  t.log('publish bundle', bundle.endoZipBase64Sha512.slice(0, 5));
   vatAdminState.installBundle(bundle.endoZipBase64Sha512, bundle);
 
-  t.log('start contract');
-  await startVstoreShop(powers, {
-    options: { [contractName]: { bundleID: bundle.endoZipBase64Sha512 } },
-  });
+  const contractOpts = { priceUnits: 10 };
+  t.log('start contract', contractName, contractOpts);
+  const opts = { bundleID: bundle.endoZipBase64Sha512, ...contractOpts };
+  await startVstoreShop(powers, { options: { [contractName]: opts } });
 
-  const {
-    consume: { zoe },
-    issuer: {
-      consume: { IST: istIssuerP },
-    },
-    instance: {
-      consume: { [contractName]: instanceP },
-    },
-  } = powers;
+  const { zoe } = powers.consume;
+  const { [contractName]: instanceP } = powers.instance.consume;
+  const { IST: istIssuerP } = powers.issuer.consume;
   const ap = E(istIssuerP).makeEmptyPurse();
   //   ap.deposit(money.mint.mintPayment(money.units(10)));
   await alice(zoe, instanceP, ap);
