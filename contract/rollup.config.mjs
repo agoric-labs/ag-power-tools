9082ed77d07c5023a11fb63eb77d9931c36e4d93
Dan Connolly <dckc@madmode.com>
2024-01-16 00:27:15 -0600

    feat: build core-eval script / permit with rollup

--- contract/rollup.config.mjs
+++ contract/rollup.config.mjs
@@ -1,9 +1,22 @@
+/**
+ * @file rollup configuration to bundle core-eval script
+ *
+ * Supports developing core-eval script, permit as a module:
+ *   - import { E } from '@endo/far'
+ *     We can strip this declaration during bundling
+ *     since the core-eval scope includes exports of @endo/far
+ *   - `bundleID = ...` is replaced using updated/cached bundle hash
+ *   - `main` export is appended as script completion value
+ *   - `permit` export is emitted as JSON
+ */
 // @ts-check
 import {
   coreEvalGlobals,
   moduleToScript,
   configureBundleID,
+  emitPermit,
 } from './tools/rollup-plugin-core-eval.js';
+import { permit } from './src/start-contractStarter.js';
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
@@ -21,6 +34,7 @@ const config = {
       cache: 'bundles',
     }),
     moduleToScript(),
+    emitPermit({ permit, file: 'deploy-starter-permit.json' }),
   ],
 };
 export default config;
