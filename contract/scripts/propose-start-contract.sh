58391662352552721bc583fcc63ecb30e2ffced6
Dan Connolly <dckc@madmode.com>
2024-01-20 22:31:36 -0600

    chore: parameterize propose-start-contract.sh

--- contract/scripts/propose-start-contract.sh
+++ contract/scripts/propose-start-contract.sh
@@ -3,18 +3,22 @@ set -xueo pipefail
 
 cd /workspace/contract
 
-SCRIPT=start-game1.js
-PERMIT=start-game1-permit.json
+SCRIPT=${SCRIPT:-start-game1.js}
+PERMIT=${PERMIT:-start-game1-permit.json}
 ls -sh "$SCRIPT" "$PERMIT"
 
 PROPOSAL=$(agd query gov proposals --output json | jq -c '.proposals | length | .+1')
 
 make fund-acct
 
+TITLE=${TITLE:-"Start Game Place Contract"}
+FROM=${FROM:-user1}
+CHAIN_ID=${CHAIN_ID:-agoriclocal}
+
 agd tx gov submit-proposal swingset-core-eval "$PERMIT" "$SCRIPT" \
-  --title="Start Game Place Contract" --description="Evaluate $SCRIPT" \
+  --title="${TITLE}" --description="Evaluate $SCRIPT" \
   --deposit=10000000ubld --gas=auto --gas-adjustment=1.2 \
-  --from user1 --chain-id agoriclocal --keyring-backend=test \
+  --from $FROM --chain-id $CHAIN_ID --keyring-backend=test \
   --yes -b block
 
 set +x # not so noisy for this part
